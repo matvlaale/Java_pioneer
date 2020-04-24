a7b97b14bed352b2e446222ea5a662806ce4d82d
matvlaale <maculaali@yandex.ru>
2020-04-24 13:02:53 +0300

    Lesson_6

--- src/Lesson_4/Cat.java
+++ src/Lesson_4/Cat.java
@@ -1,32 +1,5 @@
 package Lesson_4;
 
 public class Cat extends Animal {
-    String color;
 
-    public Cat(String name, String sex, String color) {
-        super(name, sex);
-        this.color = color;
-        normColor();
-        Main.newCat();
-        maxRun = 200;
-        maxJump = 2;
-        maxSwim = 0;
-    }
-
-    public void normColor() {
-        StringBuilder colorBuild = new StringBuilder(color);
-        colorBuild.replace(colorBuild.length() - 2, colorBuild.length(), "");
-        if (this.sex.equals("а")) this.color = colorBuild + "ая";
-        else this.color = colorBuild + "ый";
-    }
-
-    public void catInfo() {
-        if (sex.equals("а")) System.out.println(color + " кошка по имени " + name);
-        else System.out.println(color + " кот по имени " + name);
-    }
-
-    @Override
-    public void voice() {
-        System.out.println(name + " мяукнул" + sex);
-    }
 }
--- src/Lesson_4/Dog.java
+++ src/Lesson_4/Dog.java
@@ -1,32 +1,5 @@
 package Lesson_4;
 
 public class Dog extends Animal {
-    String color;
 
-    public Dog(String name, String sex, String color) {
-        super(name, sex);
-        this.color = color;
-        normColor();
-        Main.newDog();
-        maxRun = 500;
-        maxJump = 0.5f;
-        maxSwim = 10;
-    }
-
-    public void normColor() {
-        StringBuilder colorBuild = new StringBuilder(color);
-        colorBuild.replace(colorBuild.length() - 2, colorBuild.length(), "");
-        if (this.sex.equals("а")) this.color = colorBuild + "ая";
-        else this.color = colorBuild + "ый";
-    }
-
-    public void dogInfo() {
-        if (sex.equals("а")) System.out.println(color + " собака по имени " + name);
-        else System.out.println(color + " пёс по имени " + name);
-    }
-
-    @Override
-    public void voice() {
-        System.out.println(name + " гавкнул" + sex);
-    }
 }
